cd82ffd833b641f699e5974218f792832a0cf440
Arnab Banerjee <34994284+Arnab-Banerjee-90@users.noreply.github.com>
2020-12-30 21:50:49 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -31,204 +31,204 @@ function marksCalculation(){
 
 questions = [
     {
-        question: "What is the name of the Lake between Godavari and Krishna deltaic region?",
-        option1: "A. Kolleru Lake",
-        option2: "B. Pulicat Lake",
-        option3: "C. Chilika Lake",
-        option4: "D. Loktak Lake",
+        question: "Bhur of Upper Ganga Plain of India is",
+        option1: "A. Undulating, aeolian sandy deposit",
+        option2: "B. Older alluvium",
+        option3: "C. Newer alluvium",
+        option4: "D. Muddy areas",
         correct: "a"
     },
     {
-        question: "The source of the Narmada River lies in ?",
-        option1: "A. Amarkantak Plateau",
-        option2: "B. Vindhya Range",
-        option3: "C. Maikhal Range",
-        option4: "D. Palni Hills",
+        question: "In India the maximum area is covered by the",
+        option1: "A. Laterite soil",
+        option2: "B. Red Soil",
+        option3: "C. Black Soil",
+        option4: "D. Alluvial Soils",
+        correct: "d"
+    },
+    {
+        question: "The most ideal region for the cultivation of cotton in India is",
+        option1: "A. Deccan Trap Region",
+        option2: "B. The Brahmaputra Valley",
+        option3: "C. Indo-Gangetic Plain",
+        option4: "D. Rann of Kutch",
         correct: "a"
     },
     {
-        question: "Somasila Dam is located In which Indian State?",
-        option1: "A. Madhya Pradesh",
-        option2: "B. Karnataka",
-        option3: "C. Andhra Pradesh",
-        option4: "D. Maharashtra",
-        correct: "c"
-    },
-    {
-        question: "Where is Vansda National Park Located?",
-        option1: "A. Uttar Pradesh",
-        option2: "B. Gujarat",
-        option3: "C. Rajasthan",
-        option4: "D. Madhya Pradesh",
+        question: "Rain in cold weather, season is due to",
+        option1: "A. Norwesters",
+        option2: "B. Western disturbances",
+        option3: "C. Cold waves",
+        option4: "D. Arabian Sea depressions",
         correct: "b"
     },
     {
-        question: "The Fedchenko, Biafo and Hispar are the names of Himalayan",
-        option1: "A. Glaciers",
-        option2: "B. Lakes",
-        option3: "C. Hill Stations",
-        option4: "D. Trees",
-        correct: "a"
+        question: "Where is saffron cultivated in India?",
+        option1: "A. Dun Valley.",
+        option2: "B. Chambal Valley",
+        option3: "C. Karewa Terrace",
+        option4: "D. Duars Plain",
+        correct: "c"
     },
     {
-        question: "The Patkoi bum forms the boundary between",
-        option1: "A. Arunachal Pradesh and Myanmar",
-        option2: "B. Manipur and Nagaland",
-        option3: "C. Assam and Meghalaya",
-        option4: "D. Meghalaya and Nagaland",
-        correct: "a"
+        question: "The longest border of India is with",
+        option1: "A. Nepal",
+        option2: "B. Bhutan",
+        option3: "C. Afghanistan",
+        option4: "D. Bangladesh",
+        correct: "d"
     },
     {
-        question: "The fertile Bari Doab lies between?",
-        option1: "A. The Beas and the Sutlej rivers",
-        option2: "B. The Beas and the Ravi rivers ",
-        option3: "C. The Beas and the Chenub rivers",
+        question: "Muzaffarnagar in.Uttar Pradesh is famous for production of?",
+        option1: "A. Millet",
+        option2: "B. Cotton",
+        option3: "C. Sugarcane",
         option4: "D. none of the above",
-        correct: "b"
-    },
-    {
-        question: "Varanasi is located in",
-        option1: "A. Lower Ganaga plain",
-        option2: "B. Middle Ganga Plain",
-        option3: "C. Upper Ganga Plain",
-        option4: "D. Ganga-Sutlej interfluence",
-        correct: "b"
-    },
-    {
-        question: "Which one of the following is an inland river? (ontorbahi)",
-        option1: "A. Pennar",
-        option2: "B. Luni",
-        option3: "C. Narmada",
-        option4: "D. Jamuna",
-        correct: "b"
-    },
-    {
-        question: "Where do the Bhagirathi and the Alakananda meet at?",
-        option1: "A. Karnaprayag",
-        option2: "B. Rudraprayag",
-        option3: "C. Haridwar",
-        option4: "D. Devaprayag",
-        correct: "d"
-    },
-    {
-        question: "Which one of the following is the junction point of the Eastern Ghats and Western Ghats?",
-        option1: "A. Javadi",
-        option2: "B. Anaimalai",
-        option3: "C. Nilgiri",
-        option4: "D. Shevaroy",
         correct: "c"
     },
     {
-        question: "Who assumed the title of ‘Haindava Dharmoddharak’?",
-        option1: "A. Guru Ramdas",
-        option2: "B. Shivaji",
-        option3: "C. Baji Rao I",
-        option4: "D. Balaji Baji Rao",
-        correct: "b"
+        question: "Bangladesh access the enclaves in Cooch Behar through the __________ corridor",
+        option1: "A. Teen Bigha",
+        option2: "B. Darjeeling",
+        option3: "C. Jalpaiguri",
+        option4: "D. none of the above",
+        correct: "a"
     },
     {
-        question: "Who wrote Kitab-ul-Hind?",
-        option1: "A. Al-Masudi",
-        option2: "B. Al-Biruni",
-        option3: "C. Sulaiman",
-        option4: "D. Firdausi",
-        correct: "b"
-    },
-    {
-        question: "Who completed the Qutab Minar?",
-        option1: "A. Qutab-ud-Din Bakhtiyar Kaki",
-        option2: "B. Nasiruddin Qubacha",
-        option3: "C. Iltutmish",
-        option4: "D. Qutbuddin Aibak",
+        question: "Blue revolution is aimed at increasing the production of ——",
+        option1: "A. Milk",
+        option2: "B. Poultry",
+        option3: "C. Shrimp",
+        option4: "D. Indigo",
         correct: "c"
     },
     {
-        question: "‘Sati’ of Hindu women was prohibited during the reign of which Mughal emperor?",
-        option1: "A. Jahangir",
-        option2: "B. Shah Jahan",
-        option3: "C. Akbar",
-        option4: "D. Aurangzeb",
+        question: "Bamboo, rags, rice bran are the main raw materials of  —— industry",
+        option1: "A. Chemical",
+        option2: "B. Fertilizer",
+        option3: "C. Cotton",
+        option4: "D. Paper",
         correct: "d"
     },
-	{
-        question: "The Sikh Guru executed by the Mughal emperor Auranagazeb was",
-        option1: "A. Tegh Bahadur",
-        option2: "B. Nanak",
-        option3: "C. Govind Singh",
-        option4: "D. Arjun Dev",
-        correct: "a"
-    },
-	{
-        question: " The centre of Portuguese power in India was",
-        option1: "A. Cochin",
-        option2: "B. Calicut",
-        option3: "C. Bijapur",
-        option4: "D. Goa",
-        correct: "d"
-    },
-	{
-        question: "During whose reign Mughal army seized Hooghly from the Portuguese",
-        option1: "A. Akbar",
-        option2: "B. Jahangir",
-        option3: "C. Shahjahan",
-        option4: "D. Aurangzeb",
+    {
+        question: "Which state in India has the largest coal reserve",
+        option1: "A. Orissa",
+        option2: "B. Jharkhand",
+        option3: "C. Chattishgarh",
+        option4: "D. West bengal",
         correct: "c"
     },
+    {
+        question: "Which of the Act made the governor of Bengal as Governor General of India?",
+        option1: "A. Pitt’s India Act, AD 1784",
+        option2: "B. The charter Act, 1813 AD",
+        option3: "C. Charter Act, 1793 AD",
+        option4: "D. The Regulating Act, AD 1773",
+        correct: "b"
+    },
+    {
+        question: "The ambassador of Emperor James I, who reached in the court of Jahangir in 1615 was __:",
+        option1: "A. Sir James Hay",
+        option2: "B. Sir Thomas Roe",
+        option3: "C. Sir Thomas Howard",
+        option4: "D. Sir John Digby",
+        correct: "b"
+    },
+    {
+        question: "Which among the following travelers has recorded his experiences in “Kitab-i-Rehla”?",
+        option1: "A. Amir Khusro",
+        option2: "B. Ibn Battuta",
+        option3: "C. Malik Murtaza",
+        option4: "D. Al-Bayhaqi",
+        correct: "b"
+    },
+    {
+        question: "The pioneers of Ocean trade with India were______",
+        option1: "A. Dutch",
+        option2: "B. Portuguse",
+        option3: "C. British",
+        option4: "D. French",
+        correct: "b"
+    },
 	{
-        question: "Who assumed the title of ‘Gangaikondachola’?",
-        option1: "A. Rajendra I",
-        option2: "B. Rajaraja I",
-        option3: "C. Rajadhiraja I",
-        option4: "D. Kulotunga I",
+        question: "First English factory in India was biult in?",
+        option1: "A. Bombay",
+        option2: "B. Surat",
+        option3: "C. Sutanuti",
+        option4: "D. Madras",
+        correct: "b"
+    },
+	{
+        question: "Battle of Carnatic (karnataker Juddho) was fought between?",
+        option1: "A. French EIC and English EIC",
+        option2: "B. French EIC and Dutch EIC",
+        option3: "C. Dutch and the Portuguese",
+        option4: "D. None of the above",
         correct: "a"
     },
 	{
-        question: "Which King founded the Vikramaslla University  ?",
-        option1: "A. Dharmapala",
-        option2: "B. Dhruva",
-        option3: "C. Devapala",
-        option4: "D. Ballal Sen",
-        correct: "a"
-    },
-	{
-        question: "Who wrote ‘Rajatarangini",
+        question: " Who wrote ‘Rajatarangini’?",
         option1: "A. Megasthenes",
         option2: "B. Kalhana",
-        option3: "C. Al-Beruni",
-        option4: "D. Herodotus",
+        option3: "C. Herodotus",
+        option4: "D. Al-Beruni",
         correct: "b"
     },
 	{
-        question: " When was the first battle of Tarain fought?",
-        option1: "A. 1175 A.D",
-        option2: "B. 1191 A.D",
-        option3: "C. 1192 A.D",
-        option4: "D. 1206 A.D",
-        correct: "b"
+        question: "When the Hindu College was founded??",
+        option1: "A. 1800",
+        option2: "B. 1850",
+        option3: "C. 1817",
+        option4: "D. 1857",
+        correct: "c"
     },
 	{
-        question: "The decline of the Nandas at the hands of Kautilya and Chandragapta Maurya has been vividly portrayed in the Sanskrit Play",
-        option1: "A. Mrichhakatika",
-        option2: "B. Devi Chandragupta",
-        option3: "C. Mattavilasa",
-        option4: "D. Mudrarakshasa",
-        correct: "d"
-    },
-	{
-        question: "In which year did Muhammad-bin-Qasim conquer Sind?",
-        option1: "A. 712 A.D",
-        option2: "B. 715 A.D",
-        option3: "C. 718 A.D",
-        option4: "D. 721 A.D",
+        question: "Who was the Governor General during the annulment of ‘Sati’ ?",
+        option1: "A. Lord Bentinck",
+        option2: "B. Lord Hastings",
+        option3: "C. Lord Dalhousie ",
+        option4: "D. Lord Canning",
         correct: "a"
     },
 	{
-        question: " The author of ‘Kadambari‘ was",
-        option1: "A. Kshemendra",
-        option2: "B. Kalhana",
-        option3: "C. Bhababhuti",
-        option4: "D. Banabhatta",
+        question: "Kabuliyat and Patta as instruments of settlement were introduced by",
+        option1: "A. Sher-Shah",
+        option2: "B. Humayun",
+        option3: "C. Akbar",
+        option4: "D. Ibrahim Lodi",
+        correct: "a"
+    },
+	{
+        question: "The Asiatic Society of Bengal was founded by",
+        option1: "A. William Marshal",
+        option2: "B. Rammohan Roy",
+        option3: "C. Sir William Jones",
+        option4: "D. C.F Andrews",
+        correct: "c"
+    },
+	{
+        question: "Malik Kafur was general of",
+        option1: "A. Firuz Shah Tughlag",
+        option2: "B. Chandragupta Maurya",
+        option3: "C. Bimbisara",
+        option4: "D. Alauddin Khilji",
         correct: "d"
+    },
+	{
+        question: " When did Vasco-da-Gama come to India ?",
+        option1: "A. 1498 A.D",
+        option2: "B. 1490 A.D",
+        option3: "C. 1492 A.D",
+        option4: "D. 1495 A.D",
+        correct: "a"
+    },
+	{
+        question: "Second Anglo-Maratha war was fought during?",
+        option1: "A. 1810-1812",
+        option2: "B. 1815-1816",
+        option3: "C. 1802-1805",
+        option4: "D. None of the above",
+        correct: "c"
     }
 ];
 
